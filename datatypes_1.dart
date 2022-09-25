82f0c8203f24ce3246390397e6cd82a634aa2c8b
Madiha Khan <60982372+madiha1012@users.noreply.github.com>
2022-09-25 18:31:45 +0500

    Add files via upload

---
+++ datatypes_1.dart
@@ -0,0 +1,13 @@
+void main() {
+ int num1 = 10;
+  double num2 = 11;
+  num num3 = 12;
+  
+  print(num1+num2);
+  print(num3 - num1);
+  print(num2 * num3);
+  
+  //boolean
+  print(num3 == num2);
+  print(num1 != num3);
+}
